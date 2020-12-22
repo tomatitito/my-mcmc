c708aa119e14b93076b7595fb95b91e63d6e5c88
Jens Kouros <jens.kouros@posteo.de>
2020-12-22 13:13:21 +0100

    Add lazyRandomListWithState

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,22 @@
 module Main where
 
+import Control.Monad.State.Lazy
 import qualified Data.Vector.Unboxed as U
 import Kernels.MetropolisHastings
 import System.Random.MWC hiding (uniform)
-import System.Random.Stateful 
+import System.Random.Stateful
 
-import Kernels.MetropolisHastings
-
-lazyRandomList :: RandomGen g  => g -> [Int]
-lazyRandomList g = 
+lazyRandomList :: RandomGen g => g -> [Int]
+lazyRandomList g =
   x : lazyRandomList gNext
-  where (x, gNext) = uniform g
+  where
+    (x, gNext) = uniform g
 
+lazyRandomListWithState :: Int -> State StdGen [Int]
+lazyRandomListWithState = (`replicateM` randomInt)
+  where
+    randomInt :: State StdGen Int
+    randomInt = state $ uniform
 
 ioIsNotLazy :: Double -> IO [Double]
 ioIsNotLazy x = do
@@ -22,4 +27,7 @@ main :: IO ()
 main = do
   let pureGen = mkStdGen 42
   let pureRandomList = lazyRandomList pureGen
+  -- not so nice: get the list like this
+  let lazyStateList = evalState (lazyRandomListWithState 5) pureGen
   print $ take 4 $ pureRandomList
+  print lazyStateList
