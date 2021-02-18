5a96ab2baf9012300514e6621a64e3f053f950d0
Fred Roth <frederick.roth@breuninger.de>
2021-02-18 13:00:52 +0100

    Reintroduced configurable LogProb and Stepsize

--- app/Main.hs
+++ app/Main.hs
@@ -7,14 +7,16 @@ import Kernels.MetropolisHastingsLazy
 import qualified Helpers as H
 import System.Random.MWC hiding (uniform)
 import System.Random.Stateful
+import Types
 
 main :: IO ()
 main = do
   let n = 5
       initial = 1.0
       gen = mkStdGen 42
+      stepsize = Stepsize 0.9
       
-      stateRes = sampleStateT initial gen n
-      stateRes' = sampleStateT' initial gen n
+      stateRes = sampleStateT H.logProb stepsize initial gen n
+      stateRes' = sampleStateT' H.logProb stepsize initial gen n
   print stateRes
   print stateRes'
--- src/Kernels/MetropolisHastingsLazy.hs
+++ src/Kernels/MetropolisHastingsLazy.hs
@@ -2,72 +2,66 @@ module Kernels.MetropolisHastingsLazy where
 
 import Control.Monad.State.Lazy
 import System.Random
-import Data.Coerce
-import qualified Helpers
 import Types
 
-acceptanceProbability :: Double -> Double -> Double
-acceptanceProbability current proposal =
+acceptanceProbability :: LogProb -> Double -> Double -> Double
+acceptanceProbability (LogProb logProb) current proposal =
   min 1 (exp (logProb proposal - logProb current))
-  where logProb = coerce Helpers.logProb 
 
-propose :: Double -> State StdGen Double
-propose current = state $ do
+propose :: Stepsize -> Double -> State StdGen Double
+propose (Stepsize stepsize) current = state $ do
   (proposal, newGen) <- uniformR (current - stepsize, current + stepsize)
   return (proposal, newGen)
-  where
-    stepsize = 0.9
 
-propose' :: State (StdGen, Double) Double
-propose' = do
+propose' :: Stepsize -> State (StdGen, Double) Double
+propose' (Stepsize stepsize) = do
   (gen, current) <- get
   let (proposal, newGen) = uniformR (current - stepsize, current + stepsize) gen
   put(newGen, current)
   return proposal
-  where
-    stepsize = 0.9
 
-acceptOrReject :: Double -> Double -> State StdGen Double
-acceptOrReject current proposal = state $ do
+acceptOrReject :: LogProb -> Double -> Double -> State StdGen Double
+acceptOrReject logProb current proposal = state $ do
   (rand, newGen) <- uniformR (0.0, 1.0)
-  if rand < acceptanceProbability current proposal
+  if rand < acceptanceProbability logProb current proposal
    then return (proposal, newGen)
    else return (current, newGen)
 
-acceptOrReject' :: Double -> State (StdGen, Double) Double
-acceptOrReject' proposal = do
+acceptOrReject' :: LogProb -> Double -> State (StdGen, Double) Double
+acceptOrReject' logProb proposal = do
   (gen, current) <- get
   let (rand, newGen) = uniformR (0.0, 1.0) gen
-  let nextValue = if rand < acceptanceProbability current proposal
+  let nextValue = if rand < acceptanceProbability logProb current proposal
                    then proposal
                    else current
   put (newGen, nextValue)
   return nextValue
 
-transition :: Double -> State StdGen Double
-transition current =
+transition :: LogProb -> Stepsize -> Double -> State StdGen Double
+transition logProb stepsize current =
   propose' >>= acceptOrReject'
   where
-    propose' = propose current
-    acceptOrReject' = acceptOrReject current
+    propose' = propose stepsize current
+    acceptOrReject' = acceptOrReject logProb current
 
 -- State s a
 -- State (StdGen, Double) Double 
-transition' :: State (StdGen, Double) Double
-transition' = propose' >>= acceptOrReject'
+transition' :: LogProb -> Stepsize -> State (StdGen, Double) Double
+transition' logProb stepsize = propose' stepsize >>= acceptOrReject' logProb
  
-transitionStateT :: StateT Double (State StdGen) Double
-transitionStateT = StateT $ \i -> do
+transitionStateT :: LogProb -> Stepsize -> StateT Double (State StdGen) Double
+transitionStateT logProb stepSize = StateT $ \i -> do
   gen <- get
-  let (v', gen') = runState (transition i) gen
+  let (v', gen') = runState (transition logProb stepSize i) gen
   put gen'
   return (i, v') -- State Double (Double, Double)
 
-sampleStateT :: Double -> StdGen -> Int -> ([(Double, Double)], StdGen)
-sampleStateT initial gen n =
-  let innerState = runStateT transitionStateT initial
+sampleStateT :: LogProb -> Stepsize -> Double -> StdGen -> Int -> ([(Double, Double)], StdGen)
+sampleStateT logProb stepsize initial gen n =
+  let innerState = runStateT (transitionStateT logProb stepsize) initial
   in runState (replicateM n innerState) gen
 
-sampleStateT' :: Double -> StdGen -> Int -> ([Double], (StdGen, Double))
-sampleStateT' initial gen n =
-  runState (replicateM n transition') (gen, initial)
+sampleStateT' :: LogProb -> Stepsize -> Double -> StdGen -> Int -> ([Double], (StdGen, Double))
+sampleStateT' logProb stepsize initial gen n =
+  runState (replicateM n transition'') (gen, initial)
+  where transition'' = transition' logProb stepsize
